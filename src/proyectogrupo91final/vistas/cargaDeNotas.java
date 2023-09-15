da908681c7b261a057a7f193726ed5776b20c906
PabloMurriello <pablo79873@gmail.com>
2023-09-15 15:39:41 -0300

    creaciòn de vistas de acuerdo a lo solicitado

--- src/proyectogrupo91final/vistas/cargaDeNotas.java
+++ src/proyectogrupo91final/vistas/cargaDeNotas.java
@@ -9,12 +9,12 @@ package proyectogrupo91final.vistas;
  *
  * @author HP
  */
-public class ConsultaAlumnoPorMateria extends javax.swing.JPanel {
+public class cargaDeNotas extends javax.swing.JInternalFrame {
 
     /**
-     * Creates new form ConsultaAlumnoPorMateria
+     * Creates new form cargaDeNotas
      */
-    public ConsultaAlumnoPorMateria() {
+    public cargaDeNotas() {
         initComponents();
     }
 
@@ -27,16 +27,18 @@ public class ConsultaAlumnoPorMateria extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 394, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 274, Short.MAX_VALUE)
         );
+
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
 
